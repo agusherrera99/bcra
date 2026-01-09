025f5a57bb26f04f8c414a929c2f5b7fe367dfa6
agusherrera99 <agustinherrera.dev@gmail.com>
2026-01-09 08:56:47 -0300

    Add fieldnames, column_formats to sheet_map and pass to google_sheet.upload_to_sheets method

--- main.py
+++ main.py
@@ -8,25 +8,47 @@ def main():
     sheet_map = {
         'tipo_cambio_minorista': {
             'id': '184L6JQn2TZIy_5rzZbIhKA78p03BlBhVZLGXIW8Mi9o',
-            'from_date': '2019-12-31'
-            },
+            'from_date': '2019-12-31',
+            'fieldnames': ['fecha', 'valor'],
+            'column_formats': {
+                'fecha': 'date',
+                'valor': 'currency'
+            }
+        },
         'tasa_depositos_30': {
             'id': '1WCA_-_Xaq1bZcStdyVyxNUrdsdqkDLQ6hjODyH3FcIQ',
-            'from_date': '2019-12-31'
-            },
+            'from_date': '2019-12-31',
+            'fieldnames': ['fecha', 'valor'],
+            'column_formats': {
+                'fecha': 'date',
+                'valor': 'currency'
+            }
+        },
         'ipc_interanual': {
             'id': '1Cjbw6OfPJ9CkB8SmY73ZYlvc2afeMNltlBARL7TK95I',
-            'from_date': '2019-12-31'
-            },
+            'from_date': '2019-12-31',
+            'fieldnames': ['fecha', 'valor'],
+            'column_formats': {
+                'fecha': 'date',
+                'valor': 'currency'
+            }
+        },
         'ipc_mensual': {
             'id': '1bFUt87NufZOi4QbYAtO0Yz-iNKFMSfEGVEW0iMkiuOg',
-            'from_date': '2019-12-31'
+            'from_date': '2019-12-31',
+            'fieldnames': ['fecha', 'valor'],
+            'column_formats': {
+                'fecha': 'date',
+                'valor': 'currency'
             }
+        }
     }
-    
+
     for sheet_name, _map in sheet_map.items():
         sheet_id = _map.get('id', None)
         from_date = _map.get('from_date', None)
+        fieldnames = _map.get('fieldnames', [])
+        column_formats = _map.get('column_formats', {})
 
         bcra_data = monetarias.get_data(sheet_name, from_date)
 
@@ -36,8 +58,9 @@ def main():
             spreadsheet_id=sheet_id
         )
         google_sheet.upload_to_sheets(
-            fieldnames=['fecha', 'valor'],
-            data_dicts=bcra_data['results'][0]['detalle']
+            fieldnames=fieldnames,
+            column_formats=column_formats,
+            data_dicts=bcra_data
         )
 
 
